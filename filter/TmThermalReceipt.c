021190f9e3a2969264e760c2798e463f8f02c41a
Garrett Corbin <jgarrettcorbin@gmail.com>
2025-08-12 17:20:13 -0700

    test

--- filter/TmThermalReceipt.c
+++ filter/TmThermalReceipt.c
@@ -935,14 +935,14 @@ static int WriteBand(cups_page_header_t* p_header, unsigned char *p_data, unsign
 	if ( EPTMD_SUCCESS != result ) { return result; }
     result = WriteData( p_data, (unsigned int)(EPTMD_BITS_TO_BYTES(width) * lines) );
 	if ( EPTMD_SUCCESS != result ) { return result; }
+	unsigned char CommandSetPrintSpeed[7] = { GS, '(', 'K', 0x02, 0x00, 50, 10 }; 
+	result = WriteData(CommandSetPrintSpeed, sizeof(CommandSetPrintSpeed));
+	if (EPTMD_SUCCESS != result) { return result; }
 	
 	unsigned char CommandSetGraphicsdataGSpL50[7] = { GS, '(', 'L', 2, 0, 48, 50 };
 	result = WriteData( CommandSetGraphicsdataGSpL50, sizeof(CommandSetGraphicsdataGSpL50) );
 	if ( EPTMD_SUCCESS != result ) { return result; }
 
-	unsigned char CommandSetPrintSpeed[7] = { GS, '(', 'K', 0x02, 0x00, 50, 10 }; 
-	result = WriteData(CommandSetPrintSpeed, sizeof(CommandSetPrintSpeed));
-	if (EPTMD_SUCCESS != result) { return result; }
 	
 	return EPTMD_SUCCESS;
 }
